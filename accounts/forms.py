1e4969542fac6b7a99b05466f82a747a6d5a594b
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-05 20:38:48 +0900

    #1. auth앱의 PasswordChangeForm 커스텀을 통한 암호 수정 구현

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,8 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import (
+    UserCreationForm,
+    PasswordChangeForm as AuthPasswordChangeForm,
+)
 
 from accounts.models import User
 
@@ -37,3 +40,12 @@ class ProfileForm(forms.ModelForm):
             "phone_number",
             "gender",
         ]
+
+
+class PasswordChangeForm(AuthPasswordChangeForm):
+    def clean_new_password2(self):
+        old_password = self.cleaned_data.get("old_password")
+        new_password2 = super().clean_new_password2()
+        if old_password == new_password2:
+            raise forms.ValidationError("새로운 암호는 기존 암호와는 다르게 입력해주세요.")
+        return new_password2
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,16 @@
 from django.contrib import messages
 from django.contrib.auth import login as auth_login
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.views import LoginView, logout_then_login
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import (
+    LoginView,
+    logout_then_login,
+    PasswordChangeView as AuthPasswordChangeView,
+)
 from django.shortcuts import render, redirect
-from .forms import SingupForm, ProfileForm
+from django.urls import reverse_lazy
+
+from .forms import SingupForm, ProfileForm, PasswordChangeForm
 
 login = LoginView.as_view(template_name="accounts/login_form.html")
 
@@ -53,3 +60,16 @@ def profile_edit(request):
             "form": form,
         },
     )
+
+
+class PasswordChangeView(LoginRequiredMixin, AuthPasswordChangeView):
+    success_url = reverse_lazy("password_change")
+    template_name = "accounts/password_change_form.html"
+    form_class = PasswordChangeForm
+
+    def form_valid(self, form):
+        messages.success(self.request, "암호를 변경했습니다.")
+        return super().form_valid(form)
+
+
+password_change = PasswordChangeView.as_view()
