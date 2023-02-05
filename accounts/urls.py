df48772a3bd6a25cd2ee3311bee7343464a80489
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-05 13:44:52 +0900

    #1. 로그아웃시 로그인 페이지로 redirect

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,6 @@ from accounts import views
 
 urlpatterns = [
     path("login/", views.login, name="login"),  # /accounts/login/ => seetings.LOGIN_URL
+    path("logout/", views.logout, name="logout"),
     path("signup/", views.signup, name="signup"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,16 @@
 from django.contrib import messages
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, logout_then_login
 from django.shortcuts import render, redirect
 from .forms import SingupForm
 
 
 login = LoginView.as_view(template_name="accounts/login_form.html")
+
+
+def logout(request):
+    messages.success(request, "로그아웃 되었습니다.")
+    return logout_then_login(request)
+
 
 # Create your views here.
 def signup(request):
