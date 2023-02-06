cbdf17c78a36ce56fe81c0b77b242d04fad986fd
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-06 16:27:07 +0900

    #1. Timeline에 Follow하는 유저들의 포스팅 만을 노출

--- insta/views.py
+++ insta/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.shortcuts import render, redirect, get_object_or_404
 
 from insta.forms import PostForm
@@ -9,6 +10,10 @@ from insta.models import Tag, Post
 
 @login_required
 def index(request):
+    post_list = Post.objects.all().filter(
+        Q(author=request.user) | Q(author__in=request.user.following_set.all())
+    )
+
     suggested_user_list = (
         get_user_model()
         .objects.all()
@@ -19,6 +24,7 @@ def index(request):
         request,
         "insta/index.html",
         {
+            "post_list": post_list,
             "suggested_user_list": suggested_user_list,
         },
     )
